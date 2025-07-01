470f2723ce8b39d4f52dec095ceff97a53a9034a
Cleiton Barros <cleiton.junior@aluno.faculdadeimpacta.com.br>
2025-07-01 18:44:31 -0300

    first commit

---
+++ 02-ignite-timer/src/pages/Home/styles.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;    
+        gap: 3.5rem;
+    }
+`;
+
+export const CountdownContainer = styled.div`
+    font-family: 'Roboto Mono', monospace;
+    font-size: 10rem;
+    line-height: 8rem;
+    color: ${(props) => props.theme['gray-100']};
+
+    display: flex;
+    gap: 1rem;
+
+    span {
+        background: ${(props) => props.theme['gray-700']};
+        padding: 2rem 1rem;
+        border-radius: 8px;
+    }
+`;
+
+export const Separator = styled.div`
+    padding: 2rem 0;
+    color: ${(props) => props.theme['green-500']};
+
+    width: 4rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+`;
+
+export const BaseCountdownButton = styled.button`
+    width: 100%;
+    padding: 1rem;
+    border: 0;
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+    gap: 0.5rem;   
+
+    cursor: pointer;
+
+    font-weight: bold;
+ 
+    transition: background-color 0.2s;
+
+    &:disabled {
+        opacity: 0.7;
+        cursor: not-allowed;
+    }
+`;
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+    color: ${(props) => props.theme['gray-100']};
+    background: ${(props) => props.theme['green-500']};
+
+    &:not(:disabled):hover {
+        background: ${(props) => props.theme['green-700']};
+    }
+`;
+export const StoptCountdownButton = styled(BaseCountdownButton)`
+    color: ${(props) => props.theme['gray-100']};
+    background: ${(props) => props.theme['red-500']};
+
+    &:not(:disabled):hover {
+        background: ${(props) => props.theme['red-700']};
+    }
+`;
